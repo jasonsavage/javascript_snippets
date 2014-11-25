8a062b23b743e00a2a6c1f96d21642c8b2e63a6c
Jason <jason.savage2@gmail.com>
2014-11-25 14:21:31 -0500

    fixed jslint issues

--- dates/validDate.js
+++ dates/validDate.js
@@ -9,29 +9,30 @@ function validateDate( value )
     //split month, day, year
     var p = value.split('/'),
         days = [0,31,28,31,30,31,30,31,31,30,31,30,31]; //match index with m variable
-    
+
     //check correct length (3) parts
-    if( p.length != 3 ) return false;
-    
+    if( p.length != 3 ) { return false; }
+
     var m = parseInt(p[0]),
         d = parseInt(p[1]),
         y = parseInt(p[2]);
-    
+
     //check if year is between 1000 and 9999
-    if(y < 1000 || y > 9999) return false; //year is invalid
-    
+    if(y < 1000 || y > 9999) { return false; }//year is invalid
+
     //check if month is between 1 and 12
-    if( m < 1 || m > 12 ) return false; //month is invalid
-    
+    if( m < 1 || m > 12 ) { return false; }//month is invalid
+
     //check if day is between 1 and 31
-    if( d < 1 || d > 31 ) return false; //day is invalid
-    
+    if( d < 1 || d > 31 ) { return false; }//day is invalid
+
     //check for leap year
-    if( ((y % 4 == 0) && (y % 100 != 0)) || (y % 400 == 0))
+    if( ((y % 4 === 0) && (y % 100 !== 0)) || (y % 400 === 0)) {
         days[2] = 29; //set feb to 29 days on month
-        
-    if( d > days[m] ) return false; //invalid number for day based on days in each month
-    
+    }
+
+    if( d > days[m] ) { return false; }//invalid number for day based on days in each month
+
     //date is valid
     return true;
-};  
+}
