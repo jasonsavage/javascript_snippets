aa688b8e4fb5d42bf6a92c0ee587fce88e8f2098
Jason <jason.savage2@gmail.com>
2015-08-24 11:34:30 -0400

    regEx tweek

--- validation/phoneValidator.js
+++ validation/phoneValidator.js
@@ -6,8 +6,8 @@
  */
 function phoneValidator (value) {
     var exp1 = new RegExp(/^[0-9]{10}$/);
-    var exp2 = new RegExp(/^\(([0-9]{3})\) [0-9]{3}[-][0-9]{4}$/);
-    var exp3 = new RegExp(/^([0-9]{3})[.]([0-9]{3})[.]([0-9]{4})$/);
+    var exp2 = new RegExp(/^\(([0-9]{3})\)\s?[0-9]{3}[-][0-9]{4}$/);
+    var exp3 = new RegExp(/^([0-9]{3})[.-]([0-9]{3})[.-]([0-9]{4})$/);
 
     if (typeof value !== 'undefined' && value !== null) {
           return ( exp1.test(value) || exp2.test(value) || exp3.test(value) );
