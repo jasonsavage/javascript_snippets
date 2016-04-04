5ba2e9af887fa152f380ca7c848c5725110ab1cb
Jason <jason.savage2@gmail.com>
2016-04-04 16:07:28 -0400

    Create circumference.js

---
+++ numbers/circumference.js
@@ -0,0 +1,9 @@
+/**
+ * Calculates the circumference of a circle based on it's radius.
+ * 
+ * @param {number} radius
+ * @returns {number} - circumference
+ */
+function circumference (radius) {
+	return Math.PI * (radius *  2);
+}
