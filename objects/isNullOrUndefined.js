be7d0cdbbb1f68efaaa66c2e4cdf0dff5cfea1e4
Jason <jason.savage2@gmail.com>
2016-04-18 10:36:53 -0400

    Create isNullOrUndefined.js

---
+++ objects/isNullOrUndefined.js
@@ -0,0 +1,8 @@
+/**
+ * Checks if the value is either null ro undefined
+ * @param {*} value
+ * @returns {Boolean}
+ */
+function isNullOrUndefined(value) {
+    return (typeof value === 'undefined' || value === null);
+}
