aacc97658e9bb1a6d454fed1d36f0e1dcbdc8859
Jason <jason.savage2@gmail.com>
2016-05-31 12:13:53 -0400

    added fall back

--- arrays/sum.js
+++ arrays/sum.js
@@ -6,3 +6,12 @@
 function sum (array) {
     return array.reduce(function (a, b) { return a + b; });
 }
+
+//fallback
+function sum (array) {
+	var sum = 0;
+	array.forEach(function (value) {
+		sum += (isNaN(value) ? 0 : value);
+	});
+	return sum;
+}
