6aaa6dad5ffced5efcd052abe7a497f422c79986
Jason <jason.savage2@gmail.com>
2016-06-06 16:27:08 -0400

    Create prettyPrint.js

---
+++ strings/prettyPrint.js
@@ -0,0 +1,12 @@
+/**
+  * Fixes some formatting issue to make the supplied string more readable
+  * @param {string} value
+  * @returns {string}
+  */
+function prettyPrint (value) {
+ 	return value
+ 		.replace(/[-_]/g, ' ')
+ 		.replace(/([a-z])([A-Z])/g, '$1 $2')
+ 		.replace(/(\D)(\d)/g, '$1 $2')
+ 		.replace(/(\d)(\D)/g, '$1 $2');
+ }
