387d52afe1bdf8b4e58def9dfc5c35d5eac1ab1f
Jason <jason.savage2@gmail.com>
2016-06-09 16:30:58 -0400

    Update contains.js

--- strings/contains.js
+++ strings/contains.js
@@ -1,11 +1,9 @@
-"use_strict";
-
 /**
  * checks if [value] contains [search]
  * @param {string} value
  * @param {string} search
+ * @returns {boolen}
  */
-function contains(value, search)
-{
+function contains(value, search) {
     return value.indexOf(search) !== -1;
 }
