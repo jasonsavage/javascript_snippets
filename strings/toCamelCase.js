a44ad6b0303d042a75c5f93128fd697ab598b270
Jason <jason.savage2@gmail.com>
2016-06-09 16:28:56 -0400

    Update toCamelCase.js

--- strings/toCamelCase.js
+++ strings/toCamelCase.js
@@ -1,18 +1,9 @@
-"use_strict";
-
 /**
- * sets the first character of each word to uppercase and removes all spaces 
+ * Sets the first character of each word to uppercase and removes all spaces 
  * @param {string} value
- * @param {Boolean} removeSpaces
  */
-function toCamelCase(value, removeSpaces)
-{
-    removeSpaces = (removeSpaces === null) ? true : removeSpaces;
-    
-    var words = value.split(" ");
-    for(var i = 0; i < words.length; i++)
-    {
-        words[i] = $.firstToUpper( words[i] );
-    }
-    return (removeSpaces)? words.join("") : words.join(" ");
+function toCamelCase(value) {
+    return value.split(' ').map(function (word) {
+        return word.charAt(0).toUpperCase() + word.substr(1);
+    }).join("");
 }
