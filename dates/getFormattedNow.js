a48794774a97be3d4119705f0fd5e69af05175ce
Jason <jason.savage2@gmail.com>
2016-06-24 13:12:23 -0400

    Update getFormattedNow.js

--- dates/getFormattedNow.js
+++ dates/getFormattedNow.js
@@ -5,20 +5,20 @@
  * @returns {string}
  */
 function getFormattedNow () {
-	var now = new Date(),
-		h = now.getHours(),
-		ampm = h < 12 ? 'AM' : 'PM';
+    var now = new Date(),
+        h = now.getHours(),
+        ampm = h < 12 ? 'AM' : 'PM';
 
-	// h = 0 - 24, convert it to 0 - 12, but if 0, then set it to 12 (am)
-	h = h > 12 ? h - 12 : ( h === 0 ? 12 : h);
+    // h = 0 - 24, convert it to 0 - 12, but if 0, then set it to 12 (am)
+    h = h > 12 ? h - 12 : ( h === 0 ? 12 : h);
 
-	return [
-		(now.getMonth()+1), '/',
-		now.getDate(), '/',
-		now.getFullYear(), ' ',
-		h, ':',
-		now.getMinutes(), ':',
-		now.getSeconds(), ' ',
-		ampm
+    return [
+        (now.getMonth()+1), '/',
+        now.getDate(), '/',
+        now.getFullYear(), ' ',
+        h, ':',
+        now.getMinutes(), ':',
+        now.getSeconds(), ' ',
+        ampm
 	].join('');
 }
