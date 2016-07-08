8e10a5e5388e6520b2f1ce66ef38b43b88bf4e1d
Jason <jason.savage2@gmail.com>
2016-07-08 11:44:27 -0400

    add a space between V & S in VSync

--- strings/prettyPrint.js
+++ strings/prettyPrint.js
@@ -4,9 +4,10 @@
   * @returns {string}
   */
 function prettyPrint (value) {
- 	return value
- 		.replace(/[-_]/g, ' ')
- 		.replace(/([a-z])([A-Z])/g, '$1 $2')
- 		.replace(/(\D)(\d)/g, '$1 $2')
- 		.replace(/(\d)(\D)/g, '$1 $2');
+    return value
+        .replace(/[-_]/g, ' ')
+        .replace(/([a-z])([A-Z])/g, '$1 $2')
+        .replace(/([A-Z])([A-Z][a-z])/g, '$1 $2')
+        .replace(/(\D)(\d)/g, '$1 $2')
+        .replace(/(\d)(\D)/g, '$1 $2');
  }
