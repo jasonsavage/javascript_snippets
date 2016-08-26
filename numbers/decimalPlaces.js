99fb080fd018d6d9b4bfdc8754874fb8d8318a59
Jason <jason.savage2@gmail.com>
2016-08-26 16:58:02 -0400

    Update decimalPlaces.js

--- numbers/decimalPlaces.js
+++ numbers/decimalPlaces.js
@@ -1,8 +1,5 @@
 function decimalPlaces (value, places) {
-	value = ''+value; //to string
-	var i = value.indexOf('.');
-	if(i !== -1) {
-		return value.substr(0, i) + value.substr(i, places+1);
-	}
-	return value;
+    value = ''+value;
+    var i = value.indexOf('.');
+    return (i !== -1) ? value.substr(0, i) + value.substr(i, places+1) : value;
 }
