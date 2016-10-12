5a1c8561966988e95d41dae954375b7b61ed8d53
Jason <jason.savage2@gmail.com>
2016-10-12 10:37:17 -0400

    Create hypotenuse.js

---
+++ numbers/hypotenuse.js
@@ -0,0 +1,7 @@
+/**
+ * Finds the hypotenuse of a triangle
+ * From http://stackoverflow.com/questions/10140604/fastest-hypotenuse-in-javascript
+ */
+function hypotenuse (x, y) {
+    return Math.sqrt(x*x + y*y);
+}
