83e632862c67abd49a5c647ac47bc270ec234055
Jason <jason.savage2@gmail.com>
2018-06-04 10:20:37 -0400

    Update sum.js

--- arrays/sum.js
+++ arrays/sum.js
@@ -1,3 +1,5 @@
+import _ from 'underscore';
+
 /**
  * Adds all the values in an array together and returns the total
  * @param {Array} array
@@ -5,4 +7,24 @@
  */
 function sum (array) {
     return array.reduce(function (a, b) { return a + b; });
+}
+
+/**
+ * Add all the values in an array together using underscore
+ * @param {Array} [array=undefined]
+ * @returns {number}
+ */
+export function arraySum (array) {
+	if (array && array.length) {
+		return _.reduce(
+			array,
+			(memo, num) => {
+				// verify that num is not undefined, NaN, etc... as that will return an invalid result
+				return num ? memo + num : memo;
+			},
+			0
+		);
+	}
+
+	return 0;
 }
