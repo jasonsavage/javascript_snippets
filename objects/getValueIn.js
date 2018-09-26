4adbc235e3c147cec5a9390a41bebe8322dd2030
Jason <jason.savage2@gmail.com>
2018-09-26 12:41:17 -0400

    Update getValueIn.js

--- objects/getValueIn.js
+++ objects/getValueIn.js
@@ -1,29 +1,18 @@
 
 /**
- * Gets the value from a given path through a data structure
+ * Gets the value from the given path through a data structure
  * @param {object} context
- * @param {string} path
- * @return {any}
+ * @param {...string|number} path
+ * @return {*}
  *
  * @example
- * var context1 = { response: { data: { cat: 123 } } };
- * var context2 = { response: { data: [ { cat: 1 }, { cat: 12 }, { cat: 123 } ] } };
- * getValueIn(context1, 'response.data.cat');
- * getValueIn(context2, 'response.data[2].cat');
+ * var context1 = { response: { data: { foo: 123 } } };
+ * const value = getIn(context1, ['response', 'data', 'foo']);
  */
-function getValueIn(context, path) {
-    const temp = path.split('.');
+export const getValueIn = (context, ...path) => {
     let value = context;
-    while (value !== undefined && temp.length) {
-        const key = temp.shift();
-        if (/\[\d\]/.test(key)) {
-            const match = key.match(/(.*)\[(\d)\]/);
-            if (!match || match.length > 2) { return undefined; }
-            const [, arrKey, arrIdx] = match[1];
-            value = value[arrKey][parseInt(arrIdx, 10)];
-        } else {
-            value = value[key];
-        }
+    while (value !== undefined && path.length) {
+        value = value[path.shift()];
     }
     return value;
-}
+};
