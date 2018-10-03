44b6329db3560b31c2d74f0e90165a4e3be10e6c
Jason <jason.savage2@gmail.com>
2018-10-03 09:39:31 -0400

    Create regex.js

---
+++ regex.js
@@ -0,0 +1,3 @@
+const dataRegex = /^data:image/;
+const imageRegex = /(gif|jpe?g|tiff|png|svg)$/;	 const imageRegex = /(gif|jpe?g|tiff|png|svg)$/;
+const colorRegex = /^(rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)|#[0-9a-fA-F]{6}|#[0-9a-fA-F]{3}|transparent)$/i;
