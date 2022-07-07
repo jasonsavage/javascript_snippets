ae82b5d4f208abad1b4d570ac64706da41655d6f
Jason <jason.savage2@gmail.com>
2022-07-07 15:42:30 -0400

    Create calculateOrder.ts

---
+++ numbers/calculateOrder.ts
@@ -0,0 +1,8 @@
+/**
+ * Creates an array of numbers that are offset by [index], uses clamp wrap to grab numbers that are
+ * out of the range: 0 -> totalPages
+ */
+export const calculateOrder = (index: number, total: number): number[] => {
+    const nextIndex = clampWrap(index, 0, total - 1);
+    return range(0, total).map((_, i) => clampWrap(nextIndex + i, 0, total - 1));
+};
